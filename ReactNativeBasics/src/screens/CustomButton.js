5afed2d55caecc3040db38d86d20d30d54cbf933
Jaffar Abbas <54433038+jaffarabbas@users.noreply.github.com>
2022-09-19 15:10:36 +0500

    Inserted src/screens/CustomButton.js at 19/09/2022 15:10:36

---
+++ ReactNativeBasics/src/screens/CustomButton.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+const CustomButton = () => {
+  return (
+    <TouchableOpacity 
+        onPress={() => {
+            Alert.alert("hello","asdasd")
+        }}
+    >
+        <View style={styles.btnView}>
+            <Text>Custom Button</Text>
+        </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+    btnView:{
+        backgroundColor:'#fff',
+        color:'#333',
+        padding: 10,
+        textAlign: 'center',
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        marginTop: 20
+    }
+});
+
+export default CustomButton
