8a6748ab793c7fde45515f16ac31a9da5c239883
Jaffar Abbas <54433038+jaffarabbas@users.noreply.github.com>
2022-09-21 22:40:01 +0500

    Updated src/screens/CustomButton.js at 21/09/2022 22:40:01

--- ReactNativeBasics/src/screens/CustomButton.js
+++ ReactNativeBasics/src/screens/CustomButton.js
@@ -2,28 +2,28 @@ import React from 'react'
 import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 const CustomButton = () => {
-  return (
-    <TouchableOpacity 
-        onPress={() => {
-            Alert.alert("hello","asdasd")
-        }}
-    >
-        <View style={styles.btnView}>
-            <Text>Custom Button</Text>
-        </View>
-    </TouchableOpacity>
-  )
+    return (
+        <TouchableOpacity
+            onPress={() => {
+                Alert.alert("hello", "asdasd")
+            }}
+        >
+            <View style={styles.btnView}>
+                <Text>Custom Button</Text>
+            </View>
+        </TouchableOpacity>
+    )
 }
 
 const styles = StyleSheet.create({
-    btnView:{
-        backgroundColor:'#fff',
-        color:'#333',
+    btnView: {
+        backgroundColor: '#fff',
+        color: '#333',
         padding: 10,
         textAlign: 'center',
-        display:'flex',
-        alignItems:'center',
-        justifyContent:'center',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
         marginTop: 20
     }
 });
